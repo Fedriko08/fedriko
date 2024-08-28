9d22a3d8546fbfb6e21d8bca1972ba3df63308b2
fedriko <fedrikoartaruvi@gmail.com>
2024-08-28 16:05:19 +0700

    fedriko

---
+++ lib/pertemuan2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class pertemuan2 extends StatefulWidget {
+  const pertemuan2({super.key});
+
+  @override
+  State<pertemuan2> createState() => _pertemuan2State();
+}
+
+class _pertemuan2State extends State<pertemuan2> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      
+      appBar: AppBar(
+        title: Text("tes"),
+      ),
+    );
+  }
+}
